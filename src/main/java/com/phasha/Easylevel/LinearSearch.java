97693b71a1cd4e8b018b1fea8b611263958a4c9e
Karabo Phasha <mawaiskara@gmail.com>
2026-02-21 23:37:40 +0200

    implemented LinearSearch algorithm

--- src/main/java/com/phasha/Easylevel/LinearSearch.java
+++ src/main/java/com/phasha/Easylevel/LinearSearch.java
@@ -2,4 +2,14 @@ package com.phasha.Easylevel;
 
 public class LinearSearch {
 
+    public int run(int[] list, int target){
+        int result = -1;
+        for(int x = 0; x<list.length; x++){
+            if (list[x] == target){
+                result = x;
+                break;
+            }
+        }
+        return result;
+    }
 }
--- src/main/java/com/phasha/Main.java
+++ src/main/java/com/phasha/Main.java
@@ -1,8 +1,17 @@
 package com.phasha;
 
+import com.phasha.Easylevel.LinearSearch;
+
 public class Main {
 
     public static void main(String[] args){
 
+        int[] list = new int[10];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = i; // fill array with 0,1,2,...9
+        }
+
+        LinearSearch search = new LinearSearch();
+        System.out.println(search.run(list, 4));
     }
 }
