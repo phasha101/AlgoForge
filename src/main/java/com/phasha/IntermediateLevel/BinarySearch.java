95e10553d17594cdb7ea7eb14e5027a2ee3218be
Karabo Phasha <mawaiskara@gmail.com>
2026-02-22 23:32:07 +0200

    implemented BinarySearch

--- src/main/java/com/phasha/IntermediateLevel/BinarySearch.java
+++ src/main/java/com/phasha/IntermediateLevel/BinarySearch.java
@@ -1,4 +1,26 @@
 package com.phasha.IntermediateLevel;
 
+import java.util.Arrays;
+
 public class BinarySearch {
+
+    public int run(int[] list, int target){
+        int result = -1;
+        int[] newList = Arrays.stream(list).sorted().toArray();
+        int start = 0;
+        int end = newList.length-1;
+        while(result != target){
+            int mid = (int) (start+end)/2;
+            if (newList[mid] == target){
+                result = list[mid];
+            }
+            else if (target < list[mid]){
+                end = mid;
+            }
+            else{
+                start = mid;
+            }
+        }
+        return result;
+    }
 }
